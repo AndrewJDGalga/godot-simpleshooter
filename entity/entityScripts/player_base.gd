8f103a4696b8bbe7aa9538609578a0061a0cbaa6
D.Galga <ex.spake@gmail.com>
2023-12-17 15:23:17 -0600

    missile targetting raycast.

--- entity/entityScripts/player_base.gd
+++ entity/entityScripts/player_base.gd
@@ -5,4 +5,7 @@ class_name Player
 @export var move_speed:float = 50
 @export var base_health:int = 1
 @export var base_shield:int = 3
+var missile_targeting:RayCast2D = null
 
+func _ready():
+	missile_targeting = $missile_targeting
