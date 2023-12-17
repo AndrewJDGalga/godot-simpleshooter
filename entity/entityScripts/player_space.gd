10c7a31e08f437570206c97080b187b2e3cba869
D.Galga <ex.spake@gmail.com>
2023-12-17 15:18:04 -0600

    Input ifstatements for laser and missile + modified camera pos for space.

--- entity/entityScripts/player_space.gd
+++ entity/entityScripts/player_space.gd
@@ -6,3 +6,10 @@ func _physics_process(delta):
 	Input.get_action_strength("up"))
 	velocity = move_speed * dir * delta
 	move_and_collide(velocity)
+	
+	if Input.is_action_pressed("fire_laser"):
+		pass
+	
+	if Input.is_action_just_pressed("fire_missile"):
+		pass
+
