10a6cc96311238d301154b38da3b5b9ec4226337
D.Galga <ex.spake@gmail.com>
2023-12-21 15:22:54 -0600

    building out laser with timer and colorrect.

--- entity/entityScripts/player_base.gd
+++ entity/entityScripts/player_base.gd
@@ -2,7 +2,9 @@ extends Entity
 
 class_name Player
 
+@export var laser_fire_timelimit = 3.0
 var missile_targeting:RayCast2D = null
+var laser:RayCast2D = null
 
 func _ready():
 	missile_targeting = $missile_targeting
