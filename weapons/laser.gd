ead0996eaf313f244c1ece52c730c5a0977c7874
D.Galga <ex.spake@gmail.com>
2023-12-21 15:53:59 -0600

    init laser enable disable methods.

--- weapons/laser.gd
+++ weapons/laser.gd
@@ -1,3 +1,17 @@
 extends RayCast2D
 
+var sprite = null
 
+func _ready():
+	sprite = $ColorRect
+	setLaserActive(false)
+
+func setLaserActive(boolean):
+	sprite.visible = boolean
+	self.enabled = boolean
+
+func enableLaser():
+	setLaserActive(true)
+
+func disableLaser():
+	setLaserActive(false)
