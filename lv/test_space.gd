ce93008a3cb9862f3f6fe3ad1b1bae6e593c6857
D.Galga <ex.spake@gmail.com>
2023-12-28 21:59:39 -0600

    init square enemies will be summoned from.

--- lv/test_space.gd
+++ lv/test_space.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
-#@export
+@onready var summon_timer = $summon_timer
+@export var summon_dist_y:float = 40.0
+@export var summon_dist_x:float = 80.0
 var summon_square:Vector2
 
 func _ready():
-	
+	summon_square = Vector2( \
+		ProjectSettings.get_setting("display/window/size/viewport_width") + summon_dist_x, \
+		ProjectSettings.get_setting("display/window/size/viewport_height") + summon_dist_y)
+
+
