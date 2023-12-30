be44d6839b085309e6217e94bcaa41755e286a78
D.Galga <ex.spake@gmail.com>
2023-12-29 21:48:00 -0600

    instancing enemy in scene.

--- entity/unshielded_enemy.gd
+++ entity/unshielded_enemy.gd
@@ -1,2 +1,16 @@
 extends "res://entity/entityScripts/entity.gd"
 
+var destination: get = destination_get, set = destination_set
+var direction:Vector2 = Vector2.ZERO
+
+func _ready():
+	direction = Vector2(position.x - destination.x, position.y - destination.y).normalized() 
+
+func _physics_process(delta):
+	pass
+
+func destination_get():
+	return destination
+
+func destination_set(val:Vector2):
+	destination = val
--- lv/test_space.gd
+++ lv/test_space.gd
@@ -3,11 +3,17 @@ extends Node2D
 @onready var summon_timer = $summon_timer
 @export var summon_dist_y:float = 40.0
 @export var summon_dist_x:float = 80.0
+
+@export var test_enemy_ref:PackedScene = null
+
 var summon_square:Vector2
 
 func _ready():
 	summon_square = Vector2( \
 		ProjectSettings.get_setting("display/window/size/viewport_width") + summon_dist_x, \
 		ProjectSettings.get_setting("display/window/size/viewport_height") + summon_dist_y)
-
+	
+	var test_enemy = test_enemy_ref.instantiate()
+	test_enemy.destination_set(Vector2(20,20))
+	call_deferred("add_child", test_enemy)
 
