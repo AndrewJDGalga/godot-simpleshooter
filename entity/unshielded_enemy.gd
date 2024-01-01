44c07145d21e264f892367767754b5c21fd8af39
D.Galga <ex.spake@gmail.com>
2024-01-01 15:41:06 -0600

    re-implemented current enemy movement to cosine after misremembering and then correcting on how such movement works.

--- entity/unshielded_enemy.gd
+++ entity/unshielded_enemy.gd
@@ -1,16 +1,27 @@
 extends "res://entity/entityScripts/entity.gd"
 
-var destination: get = destination_get, set = destination_set
+var destination: set = destination_set
 var direction:Vector2 = Vector2.ZERO
 
+var freq = 1.0
+var amp = 200.0
+var time = 0
+var wave_effect := 0
+
 func _ready():
-	direction = Vector2(position.x - destination.x, position.y - destination.y).normalized() 
+	direction = Vector2(destination.x - position.x, destination.y - position.y).normalized()
 
 func _physics_process(delta):
-	pass
-
-func destination_get():
-	return destination
+	#if direction != Vector2.ZERO:
+		##velocity = cos(delta * freq) * amp * direction #move_speed * direction
+		#time += delta
+		#print(cos(time * freq)* amp)
+		#velocity = sin(time * freq) * amp * direction
+		#move_and_slide()
+	time += delta
+	wave_effect = cos(time * freq) * amp
+	velocity = Vector2(move_speed, wave_effect)
+	move_and_slide()
 
 func destination_set(val:Vector2):
 	destination = val
