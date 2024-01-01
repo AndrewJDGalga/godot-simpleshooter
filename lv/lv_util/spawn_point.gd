08b9e2210583055c7913f8b1d8f9a17929053444
D.Galga <ex.spake@gmail.com>
2023-12-31 18:09:37 -0600

    instanced enemy.

--- lv/lv_util/spawn_point.gd
+++ lv/lv_util/spawn_point.gd
@@ -4,6 +4,11 @@ extends Node2D
 
 @export var test_enemy:PackedScene = null
 
+
 func get_random_target()->Vector2:
-	#docs say this is safe
-	return target_positions[randi() % target_positions.size()]
+	return target_positions[randi() % target_positions.size()-1]
+
+func add_enemy(manager:Node2D, level:int):
+	var instance = test_enemy.instantiate()
+	instance.destination_set(get_random_target())
+	manager.call_deferred("add_child", instance)
