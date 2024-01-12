73b7d017c7b78bfe96c6baa6eb634ba020bbce77
D.Galga <ex.spake@gmail.com>
2024-01-12 00:04:28 -0600

    firing logic moved to state machine. Converted player to Area2D, I don't believe I need normal collisions the way this project has gone in design. Removing the complexity.

--- entity/entityScripts/player.gd
+++ entity/entityScripts/player.gd
@@ -1,9 +1,9 @@
-extends Entity
+extends Area2D
+
+enum LASER_STATES {READY, FIRING, COOLING, LOCKED}
 
 @onready var laser = $Laser
 @onready var laser_timer = $PlayerLaserTimer
-
-#@onready var cooldown_timer = $PlayerCoolTimer
 
 @export var laser_fill_rate= 50.0
 @export var laser_cooldown = 300
@@ -11,27 +11,45 @@ extends Entity
 var normal_cooldown = false
 var lock_cooldown = false
 
-#func _ready():
-	#laser_timer.set_fill_rate(50)
-	#cooldown_timer.set_fill_rate(laser_cooldown)
+var current_laser_state:LASER_STATES = LASER_STATES.READY
 
 func _physics_process(delta):
-	if Input.is_action_pressed("fire_laser") && !lock_cooldown:
-		laser_timer.increaseFill(delta, 50.0)
-		normal_cooldown = false
-	if Input.is_action_just_released("fire_laser") && !lock_cooldown:
-		normal_cooldown = true
-	if laser_timer.is_overheated():
-		lock_cooldown = true
+	match current_laser_state:
+		LASER_STATES.READY:
+			if Input.is_action_pressed("fire_laser"):
+				current_laser_state = LASER_STATES.FIRING
+		LASER_STATES.FIRING:
+			if laser_timer.is_overheated():
+				current_laser_state = LASER_STATES.LOCKED
+			if Input.is_action_pressed("fire_laser"):
+				laser_timer.increaseFill(delta, 50.0)
+			if Input.is_action_just_released("fire_laser"):
+				current_laser_state = LASER_STATES.COOLING
+		LASER_STATES.COOLING:
+			laser_timer.decreaseFill(delta, 50.0)
+			if laser_timer.is_cool():
+				current_laser_state = LASER_STATES.READY
+		LASER_STATES.LOCKED:
+			laser_timer.decreaseFill(delta, 20.0)
+			if laser_timer.is_cool():
+				current_laser_state = LASER_STATES.READY
 	
-	if normal_cooldown:
-		laser_timer.decreaseFill(delta, 50.0)
-	if lock_cooldown:
-		laser_timer.decreaseFill(delta, 20.0)
-	
-	if laser_timer.is_cool():
-		normal_cooldown = false
-		lock_cooldown = false
+	#if Input.is_action_pressed("fire_laser") && !lock_cooldown:
+		#laser_timer.increaseFill(delta, 50.0)
+		#normal_cooldown = false
+	#if Input.is_action_just_released("fire_laser") && !lock_cooldown:
+		#normal_cooldown = true
+	#if laser_timer.is_overheated():
+		#lock_cooldown = true
+	#
+	#if normal_cooldown:
+		#laser_timer.decreaseFill(delta, 50.0)
+	#if lock_cooldown:
+		#laser_timer.decreaseFill(delta, 20.0)
+	#
+	#if laser_timer.is_cool():
+		#normal_cooldown = false
+		#lock_cooldown = false
 	
 	#else:
 		#laser_timer.decreaseFill(delta)
