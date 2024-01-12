bbbc40444ffe1de696b36ba8bd494b89da35ae22
D.Galga <ex.spake@gmail.com>
2024-01-12 00:26:16 -0600

    moved individual states to functions to simplify.

--- entity/entityScripts/player.gd
+++ entity/entityScripts/player.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-enum LASER_STATES {READY, FIRING, COOLING, LOCKED}
+###movement
 
+###laser
+enum LASER_STATES {READY, FIRING, COOLING, LOCKED}
 @onready var laser = $Laser
 @onready var laser_timer = $PlayerLaserTimer
-
 @export var laser_fill_rate= 50.0
 @export var laser_empty_rate = 50.0
 @export var laser_empty_rate_punished = 20.0
 @export var laser_cooldown = 300
-
 var current_laser_state:LASER_STATES = LASER_STATES.READY
 
 func _physics_process(delta):
@@ -18,20 +18,29 @@ func _physics_process(delta):
 func firing_states(dt):
 	match current_laser_state:
 		LASER_STATES.READY:
-			if Input.is_action_pressed("fire_laser"):
-				current_laser_state = LASER_STATES.FIRING
+			firing_state_ready()
 		LASER_STATES.FIRING:
-			if laser_timer.is_overheated():
-				current_laser_state = LASER_STATES.LOCKED
-			if Input.is_action_pressed("fire_laser"):
-				laser_timer.increaseFill(dt, laser_fill_rate)
-			if Input.is_action_just_released("fire_laser"):
-				current_laser_state = LASER_STATES.COOLING
+			firing_state_firing(dt)
 		LASER_STATES.COOLING:
-			laser_timer.decreaseFill(dt, laser_empty_rate)
-			if laser_timer.is_cool():
-				current_laser_state = LASER_STATES.READY
+			firing_state_cooling(dt)
 		LASER_STATES.LOCKED:
-			laser_timer.decreaseFill(dt, laser_empty_rate_punished)
-			if laser_timer.is_cool():
-				current_laser_state = LASER_STATES.READY
+			firing_state_locked(dt)
+
+func firing_state_ready():
+	if Input.is_action_just_pressed("fire_laser"):
+		current_laser_state = LASER_STATES.FIRING
+func firing_state_firing(dt):
+	if laser_timer.is_overheated():
+		current_laser_state = LASER_STATES.LOCKED
+	if Input.is_action_pressed("fire_laser"):
+		laser_timer.increaseFill(dt, laser_fill_rate)
+	if Input.is_action_just_released("fire_laser"):
+		current_laser_state = LASER_STATES.COOLING
+func firing_state_cooling(dt):
+	laser_timer.decreaseFill(dt, laser_empty_rate)
+	if laser_timer.is_cool():
+		current_laser_state = LASER_STATES.READY
+func firing_state_locked(dt):
+	laser_timer.decreaseFill(dt, laser_empty_rate_punished)
+	if laser_timer.is_cool():
+		current_laser_state = LASER_STATES.READY
