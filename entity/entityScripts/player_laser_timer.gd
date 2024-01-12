e1ed2b5539f3e624be04d0893673c818320b7099
D.Galga <ex.spake@gmail.com>
2024-01-11 23:47:11 -0600

    cleaned up laser script.

--- entity/entityScripts/player_laser_timer.gd
+++ entity/entityScripts/player_laser_timer.gd
@@ -1,31 +1,28 @@
 extends ColorRect
 
-@onready var acc_back = $AccuracyBacking
-@export var max_fill:int = 50
+@export var max_fill:int = 32
+@export var color_change_rate:float = 1.2
 var current_fill:float = 0
 var current_fill_color:float = 1.0
 
 func increaseFill(dt, fill):
 	if current_fill < max_fill:
 		changeFill(fill, dt)
-	changeColor(-0.2,dt)
+	if current_fill_color >= 0.0:
+		changeColor(-color_change_rate,dt)
 
 func decreaseFill(dt, fill):
 	if current_fill > 0:
 		changeFill(-fill, dt)
-	changeColor(0.2,dt)
+	if current_fill_color < 1.0:
+		changeColor(color_change_rate,dt)
 
 func changeFill(amt, dt):
 	current_fill += amt * dt
 	self.size.x = current_fill
 
-#func _process(delta):
-	#changeColor(-0.2,delta)
-	#print(self.color)
-
 func changeColor(change, dt):
-	if current_fill_color <= 1.0 && current_fill_color > 0.0:
-		current_fill_color += change * dt
+	current_fill_color += change * dt
 	if current_fill_color < 0.0:
 		current_fill_color = 0.0
 	self.color = Color(1.0, current_fill_color, current_fill_color)
